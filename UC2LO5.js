b7e730acc17f6ee0e962af0933d51b27ae4ed19a
JCDayauon <demonlordex700@gmail.com>
2020-12-30 22:03:19 +0900

    first commit

---
+++ UC2LO5.js
@@ -0,0 +1,55 @@
+alert("LMS JC Dayauon");
+
+// // Put this in the console
+// var newLi = document.createElement("li");
+// var newA = document.createElemen("a");
+// var menu = document.getElementById("main-nav").getElementsByTagName("ul")[0];
+// menu.appenChild(newLI);
+// menu.appenChild(newA);
+// newA.innerHTML = "Attribute";
+// menu.insertBefore(newLi, menu.getElementsByTagName("li")[0]);
+
+// // Console DOM remover
+
+// var parent = document.getElementByID ("main-nav").getElementsByTagName("ul")[0];
+// var Child = parent.getElementsByTagName("li")[0];
+// var removed = parent.removeChild(child);
+// parent.appendChild(removed);
+
+
+// // // Events
+
+// var title = document.getElementByID("page-title");
+
+// title.onclick = function(){
+// 	alert("Hello There");
+// };
+
+// function(){
+//     alert("Hello There");
+// }
+
+// title.onmouseover = function(){
+//     alert("hey click me already;)");
+// }
+function setupEvents() {
+    var content = document.getElementsByClassName("card-title");
+var content = document.getElementsByClassName("show-more");
+button.onclick = function(){
+    if(card-title.className == "open"){
+        content.className = "";
+        buttton.innerHTML = "Show-more";
+    }
+            else{
+                content.className = "open";
+                button.innerHTML = "Show Less";
+
+            }
+        }
+    };
+
+
+
+window.onload = function(){
+
+};
